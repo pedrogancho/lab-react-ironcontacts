3c7f8093d9ce4cf059a95bd8f1aa269904811a75
Pedro Gancho <pedro.gancho@inlifeportugal.com>
2021-11-30 18:17:52 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
+import contactsJSON from "./contacts.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [contacts, setContacts] = useState(contactsJSON.slice(0,5));
+  function getRandomContact() {
+    const random = Math.floor(Math.random() * contactsJSON.length);
 
+    const randomContact = contactsJSON[random];
+
+    setContacts(contacts.concat(randomContact));
+  }
+  return <div className="App">
+  <h1>IronContacts</h1>
+
+  <button onClick={() => getRandomContact()}>Add Random Contact</button>
+
+  <table>
+  <tr>
+    <th>Picture</th>
+    <th>Name</th>
+    <th>Popularity</th>
+    <th>Won an Oscar</th>
+    <th>Won an Emmy</th>
+
+  </tr>
+
+  {contacts.map((celebrity) =>{
+    return(
+  <tr>
+  <td> 
+    <img 
+    src={celebrity.pictureUrl} 
+    alt="Artist" 
+    width="100" 
+    height="100" />
+    </td>
+    <td> {celebrity.name}</td>
+    <td>{celebrity.popularity.toFixed(2)}</td>
+    <td> {celebrity.wonOscar === true ? "🏆" : "" }</td>
+    <td> {celebrity.wonEmmy === true ? "🏆" : "" }</td>
+  </tr>
+  )})}  
+</table>
+  
+  </div>;
+}
 export default App;
